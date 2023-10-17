2b77931052d4d84c5375a3d79a36e33a2b995a6d
Wojciech01~ <wojciechchmura98@gmail.com>
2023-10-17 02:05:22 +0200

    added recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://ocdn.eu/pulscms-transforms/1/xGVk9kuTURBXy9mZTYwNjBlNy1lYTcyLTQ1MGYtODE1ZC1kY2IwODUzMDUyMDIuanBlZ5KVAwAVzQPozQIzkwXNBLDNAoXeAAGhMAE' )
+  ]
 
 }
